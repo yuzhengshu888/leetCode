a1207b04184f4b0135baa85dcdd3112cc9b8da5b
yuzhengshu <zhengyu.yu@myt11.com>
2021-04-27 14:28:30 +0800

    938. 二叉搜索树的范围和

---
+++ src/algorithm/sum27/Solution.java
@@ -0,0 +1,40 @@
+package algorithm.sum27;
+
+/**
+ * @description: 938. 二叉搜索树的范围和
+ * @Author: yuzhengshu
+ * @Date: 2021/4/27 13:45
+ *
+ * 给定二叉搜索树的根结点 root，返回值位于范围 [low, high] 之间的所有结点的值的和。
+ * 输入：root = [10,5,15,3,7,null,18], low = 7, high = 15
+ * 输出：32
+ */
+public class Solution {
+    public static int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+        int res = 0;
+        if (root.val <= high && root.val >= low) {
+            res = root.val;
+        }
+        if (root.val >= low) {
+            res = res + rangeSumBST(root.left, low, high);
+        }
+        if (root.val <= high) {
+            res = res + rangeSumBST(root.right, low, high);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(18, null, null);
+        TreeNode root2 = new TreeNode(7, null, null);
+        TreeNode root3 = new TreeNode(3, null, null);
+        TreeNode root4 = new TreeNode(5, root3, root2);
+        TreeNode root5 = new TreeNode(15, null, root1);
+        TreeNode root6 = new TreeNode(10, root4, root5);
+        System.out.println(rangeSumBST(root6, 7, 15));
+
+    }
+}
---
+++ src/algorithm/sum27/TreeNode.java
@@ -0,0 +1,25 @@
+package algorithm.sum27;
+
+/**
+ * @description:
+ * @Author: yuzhengshu
+ * @Date: 2021/4/27 13:46
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
