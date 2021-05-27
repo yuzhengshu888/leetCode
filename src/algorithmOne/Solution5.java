ca402f774f572c829286b6057181e54132551bef
yuzhengshu <zhengyu.yu@myt11.com>
2021-05-27 11:54:25 +0800

    461. 汉明距离

--- src/algorithmOne/Solution5.java
+++ src/algorithmOne/Solution5.java
@@ -55,6 +55,22 @@ public class Solution5 {
         }
         return ret;
     }
+
+    /**
+     * Brian Kernighan 算法
+     * 记 f(x)f(x) 表示 xx 和 x-1x−1 进行与运算所得的结果（即 f(x)=x&(x-1))，那么 f(x)f(x) 恰为 xx 删去其二进制表示中最右侧的 11 的结果。
+     * f(x)=x&(x-1) = x删除最右位1的结果
+     */
+    public int hammingDistance3(int x, int y) {
+        int s = x ^ y, ret = 0;
+        while (s != 0) {
+            s &= s - 1;
+            ret++;
+        }
+        return ret;
+    }
+
+
     public static void main(String[] args) {
         System.out.println(hammingDistance1(1, 4));
     }
