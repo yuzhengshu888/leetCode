35509f8c1b30810305edea3da9087774908cc341
zhengshu.yu <A8699787e900>
2021-05-30 12:30:10 +0800

    303. 区域和检索 - 数组不可变

---
+++ src/algorithmOne/NumArray.java
@@ -0,0 +1,31 @@
+package algorithmOne;
+
+/**
+ * @description: 303. 区域和检索 - 数组不可变
+ * @Author: yuzhengshu
+ * @Date: 2021年5月30日 12:17
+ */
+public class NumArray {
+    int[] sums;
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        sums = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2, 0, 3, -5, 2, -1};
+        NumArray numArray = new NumArray(nums);
+        System.out.println(numArray.sumRange(0, 2));
+        System.out.println(numArray.sumRange(2, 5));
+        System.out.println(numArray.sumRange(0, 5));
+    }
+}
