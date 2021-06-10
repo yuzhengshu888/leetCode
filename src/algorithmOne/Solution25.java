2c0d9fab72778815c31a12e29d12dc7889f84430
yuzhengshu <zhengyu.yu@myt11.com>
2021-06-10 18:11:11 +0800

    4.寻找两个正序数组的中位数

---
+++ src/algorithmOne/Solution25.java
@@ -0,0 +1,53 @@
+package algorithmOne;
+
+/**
+ * @description:
+ * 寻找两个正序数组的中位数
+ * @Author: yuzhengshu
+ * @Date: 2021年6月10日 17:09
+ */
+public class Solution25 {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int i1 = 1;
+        int i2 = 1;
+        int length1 = nums1.length;
+        int length2 = nums2.length;
+        int sum = length2 + length1;
+        int flag = sum % 2;
+        int idx = sum / 2;
+        while (i1 <= length1 && i2 <= length2) {
+            double num1 = nums1[i1 - 1];
+            double num2 = nums2[i2 - 1];
+            if (i1 + i2 >= idx + 2) {
+                if (flag == 1) {
+                    return Math.max(num1, num2);
+                } else {
+                    return (num1 + num2) / 2;
+                }
+            }
+            if (num1 <= num2) {
+                i1++;
+            } else {
+                i2++;
+            }
+        }
+        if (i1 >= length1) {
+            if (flag == 1) {
+                return nums2[idx - i1 + 1];
+            } else {
+                return (double) (nums2[idx - i1] + nums2[idx - i1 + 1]) / 2;
+            }
+        }
+        if (flag == 1) {
+            return nums1[idx - i2 + 1];
+        } else {
+            return (double) (nums1[idx - i2] + nums1[idx - i2 + 1]) / 2;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 10};
+        int[] nums2 = {2, 3, 4};
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+    }
+}
