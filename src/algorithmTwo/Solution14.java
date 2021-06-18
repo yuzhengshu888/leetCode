c1331f121df9d48fbc8dae5c66dd7cdb742921e9
yuzhengshu <zhengyu.yu@myt11.com>
2021-06-18 18:06:15 +0800

    649. Dota2 参议院

--- src/algorithmTwo/Solution14.java
+++ src/algorithmTwo/Solution14.java
@@ -60,6 +60,50 @@ public class Solution14 {
         return countD > 0 ? "Dire" : "Radiant";
     }
 
+    public static String predictPartyVictory1(String senate) {
+        char[] chars = senate.toCharArray();
+        int length = chars.length;
+        char delChar;
+        int i = 0;
+        boolean isEnd = false;
+        while (!isEnd) {
+            if (i + 1 > length) {
+                i = 0;
+            }
+            delChar = chars[i] == 'R' ? 'D' : 'R';
+            //往后找
+            boolean afterHas = false;
+            for (int j = i + 1; j < length; j++) {
+                if (chars[j] == delChar) {
+                    afterHas = true;
+                    list.remove(j);
+                    if (delChar == 'D') {
+                        countD--;
+                    } else {
+                        countR--;
+                    }
+                    break;
+                }
+            }
+            if (!afterHas) {
+                for (int j = 0; j < i; j++) {
+                    if (chars[j] == delChar) {
+                        list.remove(j);
+                        if (delChar == 'D') {
+                            countD--;
+                        } else {
+                            countR--;
+                        }
+                        i--;
+                        break;
+                    }
+                }
+            }
+            i++;
+        }
+        return countD > 0 ? "Dire" : "Radiant";
+    }
+
     public static void main(String[] args) {
         String str = "RRDDDD";
         System.out.println(predictPartyVictory(str));
