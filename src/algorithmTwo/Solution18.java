0a66b99a8a3317cfc086c073f1d8fb0b2dcd28e2
yuzhengshu <zhengyu.yu@myt11.com>
2021-06-21 14:27:48 +0800

    191. 位1的个数

---
+++ src/algorithmTwo/Solution18.java
@@ -0,0 +1,12 @@
+package algorithmTwo;
+
+/**
+ * @description: 191. 位1的个数
+ * @Author: yuzhengshu
+ * @Date: 2021年6月21日 14:22
+ */
+public class Solution18 {
+    public int hammingWeight(int n) {
+        return Integer.bitCount(n);
+    }
+}
