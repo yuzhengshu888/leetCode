d0d88a813ab837df230bac0c0e402c4fdc62c7fc
yuzhengshu <zhengyu.yu@myt11.com>
2021-06-24 17:45:03 +0800

    98. 验证二叉搜索树

--- src/algorithmTwo/Solution25.java
+++ src/algorithmTwo/Solution25.java
@@ -50,6 +50,27 @@ public class Solution25 {
         return true;
     }
 
+    /**
+     * 递归版
+     */
+    long pre = Long.MIN_VALUE;
+    public boolean isValidBST3(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        // 访问左子树
+        if (!isValidBST3(root.left)) {
+            return false;
+        }
+        // 访问当前节点：如果当前节点小于等于中序遍历的前一个节点，说明不满足BST，返回 false；否则继续遍历。
+        if (root.val <= pre) {
+            return false;
+        }
+        pre = root.val;
+        // 访问右子树
+        return isValidBST3(root.right);
+    }
+
     public static void main(String[] args) {
         Integer[] nums = {2, 1, 3};
         TreeNode root = TreeNode.createTreeNode(nums);
