b6732a581d7d0be1120ccf2821552df44b14fd82
yuzhengshu <zhengyu.yu@myt11.com>
2021-07-01 11:06:44 +0800

    1560. 圆形赛道上经过次数最多的扇区

--- src/algorithmThree/Solution9.java
+++ src/algorithmThree/Solution9.java
@@ -3,7 +3,7 @@ package algorithmThree;
 import java.util.*;
 
 /**
- * @description: 该马拉松全程由
+ * @description: 1560. 圆形赛道上经过次数最多的扇区
  * @Author: yuzhengshu
  * @Date: 2021年6月30日 17:32
  */
@@ -29,6 +29,26 @@ public class Solution9 {
         }
         return res;
     }
+
+    public List<Integer> mostVisited1(int n, int[] rounds) {
+        List<Integer> ret = new ArrayList<Integer>();
+        int length = rounds.length;
+        int start = rounds[0], end = rounds[length - 1];
+        if (start <= end) {
+            for (int i = start; i <= end; i++) {
+                ret.add(i);
+            }
+        } else { // 由于题目要求按扇区大小排序，因此我们要将区间分成两部分
+            for (int i = 1; i <= end; i++) {
+                ret.add(i);
+            }
+            for (int i = start; i <= n; i++) {
+                ret.add(i);
+            }
+        }
+        return ret;
+    }
+
 
     public static void main(String[] args) {
         int[] nums = {16, 8};
