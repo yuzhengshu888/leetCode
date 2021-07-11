139f09ffe010a7c1de53088df95e355a300ed04b
zhengshu.yu <A8699787e900>
2021-07-11 21:52:18 +0800

    274. H 指数

---
+++ src/algorithmThree/Solution26.java
@@ -0,0 +1,36 @@
+package algorithmThree;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class Solution26 {
+    public static int hIndex(int[] citations) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int length = citations.length;
+        for (int citation : citations) {
+            map.merge(Math.min(citation, length), 1, Integer::sum);
+        }
+
+        int sum = 0;
+        int hIndex = 0;
+        for (int i = length; i > 0; i--) {
+            Integer reference = map.get(i);
+            if (reference == null) {
+                if (sum >= i) {
+                    return i;
+                }
+                continue;
+            }
+            sum += reference;
+            if (sum >= i) {
+                return i;
+            }
+        }
+        return hIndex;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {11,15};
+        System.out.println(hIndex(nums));
+    }
+}
